bd66c67f7fe2a7adb002b69bc894389ba7937e7a
mhafizkn <hafizkn98@gmail.com>
2022-01-20 17:58:32 +0800

    :sparkles: Added Vue UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -125,8 +125,13 @@
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
+                    {{-- Vue Component --}}
+                    <div id="app">
+                        <example-component></example-component>
+                     </div>
                 </div>
             </div>
         </div>
     </body>
+    <script src="{{ mix('/js/app.js') }}"></script>
 </html>
